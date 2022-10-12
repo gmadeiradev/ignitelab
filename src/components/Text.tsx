55e160dce59dead5251808a7055f4056ba89a8eb
Gabriel da Silva Madeira <gsmmadeira1@gmail.com>
2022-10-12 07:46:19 -0300

    feat: /src/components/ add text component - base component

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,13 +1,19 @@
-import { clsx } from 'clsx';
+import { Slot } from '@radix-ui/react-slot';
 
-interface TextProps {
+import { clsx } from 'clsx';
+import { ReactNode } from 'react';
+
+export interface TextProps {
     size?: 'sm' | 'md' | 'lg';
-    children: string;
+    children: ReactNode;
+    asChild: boolean;
 }
 
-export function Text({ size = 'md', children }: TextProps) {
+export function Text({ size = 'md', children, asChild }: TextProps) {
+    const Comp = asChild ? Slot : 'span';
+
     return (
-        <span className={clsx('text-gray-100 font-sans',
+        <Comp className={clsx('text-gray-100 font-sans',
             {
                 'text-xs': size == 'sm',
                 'text-md': size == 'md',
@@ -16,6 +22,6 @@ export function Text({ size = 'md', children }: TextProps) {
         )}
         >
             {children}
-        </span>
+        </Comp>
     )
 };
