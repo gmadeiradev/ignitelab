100b8f9c787532f880b7ddddf9386e33e688cd76
gmadeiradev <gsmmadeira1@gmail.com>
2022-10-14 11:30:30 -0300

    feat: create form style and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,72 @@
 import './styles/global.css';
 
+import { Envelope, Lock, TextT } from 'phosphor-react';
+
+import { Logo } from './assets/Logo';
+import { Text } from './components/Text/Text';
+import { Button } from './components/Button/Button';
+import { Heading } from './components/Heading/Heading';
+import { Checkbox } from './components/Checkbox/Checkbox';
+import { TextInput } from './components/TextInput/TextInput';
+
 export function App() {
   return (
-    <>
-      <h1 className='font-bold text-2xl text-violet-500'>Hello World</h1>
+    <div className='w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100'>
+      <header className='flex flex-col items-center '>
+        <Logo />
+        <Heading size='lg' className='mt-4'>
+          Ignite Lab
+        </Heading>
+        <Text size='lg' className='text-gray-400 mt-1'>
+          Faça login e comece a usar
+        </Text>
+      </header>
+      {/* form */}
+      <form className='flex flex-col gap-4 items-stretch w-full max-w-sm mt-10'>
+        {/* email input */}
+        <label htmlFor='email' className='flex flex-col gap-3'>
+          <Text className='font-semibold'>
+            Endereço de e-mail
+          </Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Envelope />
+            </TextInput.Icon>
+            <TextInput.Input id='email' type='email' placeholder='Digite seu e-mail' />
+          </TextInput.Root>
+        </label>
+        {/* password input */}
+        <label htmlFor='password' className='flex flex-col gap-3'>
+          <Text className='font-semibold'>
+            Sua senha
+          </Text>
+          <TextInput.Root>
+            <TextInput.Icon>
+              <Lock />
+            </TextInput.Icon>
+            <TextInput.Input id='password' type='password' placeholder='********' />
+          </TextInput.Root>
+        </label>
+        {/* checkbox input */}
+        <label htmlFor="remember" className='flex items-center gap-2'>
+          <Checkbox id='remember' />
+          <Text size='sm' className='text-gray-200'>Lembrar de mim por 30 dias</Text>
+        </label>
+        {/* button submit */}
+        <Button type='submit' className='mt-4'>
+          Entrar na plataforma
+        </Button>
+      </form>
 
-      <button className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-cyan-300'>
-        Enviar
-      </button>
-    </>
+      <footer className='flex flex-col items-center gap-4 mt-8'>
+        <Text asChild size='sm'>
+          <a href="" className='text-gray-400 underline hover:text-gray-200'>Esqueceu sua senha?</a>
+        </Text>
+
+        <Text asChild size='sm'>
+          <a href="" className='text-gray-400 underline hover:text-gray-200'>Não possui uma conta? Crie uma agora!</a>
+        </Text>
+      </footer>
+    </div>
   )
 }
