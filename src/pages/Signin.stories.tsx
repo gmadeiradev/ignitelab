4e64b3f84e8efd519183bdb7618157d673826bc4
Gabriel da Silva Madeira <gsmmadeira1@gmail.com>
2022-10-16 08:07:14 -0300

    feat: /src/pages/ create pages and add signin component

---
+++ src/pages/Signin.stories.tsx
@@ -0,0 +1,11 @@
+import { Signin } from './Signin'
+import { Meta, StoryObj } from '@storybook/react';
+
+export default {
+    title: 'Pages/Sign In',
+    component: Signin,
+    args: {},
+    argTypes: {}
+} as Meta;
+
+export const Default: StoryObj = {}
